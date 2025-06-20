52b4ce622844e2d1e351ab0bc02ef4bdde404645
ssshaise <ruchir412393@gmail.com>
2025-06-21 01:35:33 +0530

    UPDATE

--- src/App.jsx
+++ src/App.jsx
@@ -52,21 +52,21 @@ const projects = [
         title: "ReasearchGpt",
         description: "Built and deployed an AI chatbot using LangChain and Gemini-Pro for fast, accurate document analysis across PDFs, images, and text with FAISS-based retrieval.",
         tags: ["Python", "LangChain", "Streamlit", "FAISS"],
-        image: "https://i.postimg.cc/RZ3G6F2B/458107c8-07b5-46ce-91e8-ff2c9a59de4e.png",
+        image: "researchgpt.png",
         repoUrl: "https://github.com/ssshaise/ruchir-srivastava-wasserstoff-AiInternTask"
     },
     {
         title: "PlantGuard",
         description: "Built a high-accuracy plant disease detection system using EfficientNet and DeepLabV3, achieving 92% classification accuracy with custom-labeled, augmented data and precise leaf segmentation.",
         tags: ["Python", "OpenCV","Tensorflow", "EfficientNet"],
-        image: "https://i.postimg.cc/9QVNDTyq/c8030c2d-c0eb-4206-acbe-7f6086561408.png",
+        image: "plantguard.png",
         repoUrl: "https://github.com/ssshaise/PlantGuard"
     },
     {
         title: "Deepfake Detector with XAI",
         description : "Designed a multi-modal deepfake and disinformation detection system integrating visual and textual data, achieving a 92% F1-score with interpretability via SHAP and LIME.",
         tags : ["Python", "PyTorch", "OpenCV", "BERT" , "SHAP", "LIME"],    
-        image: "https://i.postimg.cc/kgQGcrVp/f2e1bf77-c5d6-4062-8a7c-c246148f45fa.png",
+        image: "deepfakedet.png",
         repoUrl: "https://github.com/ssshaise/Deepfake-Disinformation-Detector"
     },
 ];
@@ -141,7 +141,7 @@ const latestPosts = [
     {
         date: "May 20, 2024",
         title: "Integrating OpenAI API with a Streamlit App",
-        image: "https://i.postimg.cc/kXP1BZS5/Screenshot-2025-06-20-035237.png"
+        image: "post.png"
     }
 ];
 
@@ -427,10 +427,10 @@ export default function App() {
 
     const themeConfig = {
         light: {
-            bgImage: 'https://i.postimg.cc/LsV0VB1L/uwp4001574.png',
+            bgImage: 'light.png',
         },
         dark: {
-            bgImage: 'https://i.postimg.cc/5tmc7QGd/wp9618774.jpg',
+            bgImage: 'dark.jpg',
         },
     };
     
